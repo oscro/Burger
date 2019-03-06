f04e5b349ee714a4756e481405c8d381f400087a
Oscro <oscar.orchilles@gmail.com>
2019-03-06 08:15:01 -0500

    lj

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 ### Schema
 
+--LOCALHOST INIT
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
@@ -10,3 +11,18 @@ CREATE TABLE burgers
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+-- JAWSDB Init
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(250) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Colossus', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('The Maximus', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Hierarchy', false);
